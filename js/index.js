329325f29525417b6a1837bb7388e3713d073b47
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 02:29:54 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// use this to copy code snippets or us your browser's console
+function greet() {
+  console.log(`my name is ${this.name}, hi!`);
+}
+
+greet();
+
+let person = {
+  name: 'Bob',
+  greet: greet 
+};
+
+person.greet();
